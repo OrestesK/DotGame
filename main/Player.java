027c3f95bed16d2f5d4b01178c4ada9ddfcf5c33
OrestesK <oresteskassian@gmail.com>
2020-10-21 17:56:21 -0400

    yes

--- main/Player.java
+++ main/Player.java
@@ -2,9 +2,7 @@ package main;
 
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.MouseInfo;
-import java.awt.Rectangle;
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 import java.util.Random;
